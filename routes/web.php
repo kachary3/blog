9c21a7cb5e34031116d53fca5bcce3442a6e95b0
Kalpana Acharya <kalpanaa559@gmail.com>
2024-07-21 22:54:33 +1000

    Routes resources for post are defined

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::get('/',function(){
+    return view('welcome');
+});
 // Other routes...
 
 // Define resource routes for PostController
