0b7ac6004f19def1b2f91b0f64eb9143662a8cce
Kalpana Acharya <kalpanaa559@gmail.com>
2024-07-21 23:23:13 +1000

    Post model created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
         $this->call(PostsTableSeeder::class);
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>My First Blog </title>
+    <title>My First Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+         /* Custom styles for pagination arrows */
+         .pagination .page-item .page-link svg {
+            width: 16px;  
+            height: 16px; 
+        }
+
+        /* Tailwind-specific classes */
+        .w-5.h-5 {
+            width: 16px;  
+            height: 16px; 
+        }
+        
+    </style>
 </head>
 <body>
     <div class="container">
